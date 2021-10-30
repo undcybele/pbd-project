a18c5654151e0e9861f8c85ff026eb2dfa33fd2a
undcybele <andreea1bolea@gmail.com>
2021-10-30 16:51:11 +0300

    Hid firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,21 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import {env} from "./env";
 
 export const environment = {
+  firebaseConfig: {
+    apiKey: env.apiKey,
+    authDomain: env.authDomain,
+    projectId: env.projectId,
+    storageBucket: env.storageBucket,
+    messagingSenderId: env.messagingSenderId,
+    appId: env.appId,
+    measurementId: env.measurementId
+  },
   production: false
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
