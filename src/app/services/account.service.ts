7f423ee0607d41abd6d2fd3e042954ca46bc8b08
undcybele <andreea1bolea@gmail.com>
2021-11-11 22:02:29 +0200

    Added new service functions for accounts

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from "@angular/fire/compat/firestore";
-import {AccountModel} from "../models/account.model";
+import {AccountModel, accTypeEnum} from "../models/account.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   dbPath = "accounts"
-  constructor(private afs: AngularFirestore) { }
+
+  constructor(private afs: AngularFirestore) {
+  }
+
   createAcc(acc: AccountModel) {
     this.afs.collection<AccountModel>(this.dbPath).add(acc)
   }
-  getAcc(){
+
+  getAcc() {
     return this.afs.collection<AccountModel>(this.dbPath);
   }
-}
 
+  getAccByAccId(accountId: number) {
+    return this.afs.collection<AccountModel>(this.dbPath, ref => ref.where('accountId', '==', accountId).limit(1));
+  }
+
+  //for task d
+  updateSoldCurr(id: string, processedSum: number) {
+    const data = {"soldCurr": processedSum};
+    return this.afs.collection(this.dbPath).doc(id).set(data);
+  }
+
+  //for task h
+  getAllByType(searchedAccType: accTypeEnum) {
+    return this.afs.collection<AccountModel>(this.dbPath, ref => ref.where('accType', '==', searchedAccType));
+  }
+}
 
