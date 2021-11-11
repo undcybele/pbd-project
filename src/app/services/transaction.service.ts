400a9e2352ffbd4e0698d941b5e32ade26b2dba6
undcybele <andreea1bolea@gmail.com>
2021-11-11 22:02:45 +0200

    Added new service functions for transactions

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { TransactionModel } from '../models/transaction.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
+  dbPath = "transactions"
+  constructor(private afs: AngularFirestore) { }
 
-  constructor() { }
+  createTransactions(transaction: TransactionModel) {
+    this.afs.collection<TransactionModel>(this.dbPath).add(transaction)
+  }
+  getTransactions(){
+    return this.afs.collection<TransactionModel>(this.dbPath);
+  }
+
+  //for task e
+  // getAllForAcc(id: string){
+  //   let debAccs = this.afs.collection<TransactionModel>(this.dbPath, ref => ref.where('idDebAcc', '==', id))
+  //   let credAccs = this.afs.collection<TransactionModel>(this.dbPath, ref => ref.where('idCredAcc', '==', id))
+  //   return 0
+  // }
 
   executeTransaction(debAccId: number, credAccId: number, processedSum: number){
 
