213b19713078e111385686d5d0d2eeaa9e1f8788
Stefan Cosmescu <stefan.cosmescu@student.upt.ro>
2021-11-11 21:53:07 +0200

    updated html + css + ts

--- src/app/components/transaction/transaction.component.ts
+++ src/app/components/transaction/transaction.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction.component.scss']
 })
 export class TransactionComponent implements OnInit {
-
-  constructor() { }
+  transactionForm = new FormGroup({
+    credAccNumber = new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    debAccNumber = new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    sum = new FormControl(0, [Validators.required, Validators.pattern('[0-9]{5}')]),
+    desc = new FormControl('', [Validators.required, Validators.maxLength(10)])
+  })
+  transactions: TransactionModel[]
+  constructor(
+      private accService: TransactionService,
+    ) { }
 
   ngOnInit(): void {
   }
 
 }
+
