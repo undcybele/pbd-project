c8888ec7565cfb26900136a5b387bd49f0dc965f
undcybele <andreea1bolea@gmail.com>
2021-11-16 21:31:59 +0200

    Added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accounts', component: AccountComponent },
+  { path: 'transactions', component: TransactionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
