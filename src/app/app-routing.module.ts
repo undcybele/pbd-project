67467945ae533556fa59d0942399e38a8438b73d
Stefan Cosmescu <stefan.cosmescu@student.upt.ro>
2021-12-10 19:32:27 +0200

    added route for account-transaction component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './components/account/account.component';
 import { TransactionComponent } from './components/transaction/transaction.component';
+import {AccountTransactionComponent} from "./components/account-transaction/account-transaction.component";
 
 const routes: Routes = [
   { path: 'accounts', component: AccountComponent },
   { path: 'transactions', component: TransactionComponent },
+  { path: 'account-transactions', component: AccountTransactionComponent }
 ];
 
 @NgModule({
